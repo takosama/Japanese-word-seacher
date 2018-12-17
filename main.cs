020d2033dde3d82339d22c518ad4a2b6a6f75989
takosama <takosama.312@gmail.com>
2018-12-17 23:35:35 +0900

    Update main.cs

--- main.cs
+++ main.cs
@@ -89,7 +89,7 @@ namespace 類義語とか調べるやつ
 
             public List<string> WordidArray =>
                          Enumerable.Range(0, _input.Count)
-                         .Select(x => (_input[x] ))
+                         .Select(x => (_input[x]))
                          .ToList();
         }
         public class SearchSenceSynSetResult
@@ -114,15 +114,15 @@ namespace 類義語とか調べるやつ
             cn.Open();
         }
 
-        public SearchWordNamReseult SearchWordName(string wordid)
+        public SearchWordResult SearchWordName(string wordid)
         {
             var cmd = cn.CreateCommand();
             cmd.CommandText = $"SELECT * FROM word WHERE wordid={wordid}";
             var reader = cmd.ExecuteReader();
 
-            string[] keys = { "lemma" };
-            var list = reader.GetValueList(keys).Select(x=>x[0]).ToList();
-            return new SearchWordNamReseult(list);
+            string[] keys = { "wordid", "lemma", "pos" };
+            var list = reader.GetValueList(keys);
+            return new SearchWordResult(list);
         }
 
         public SearchWordResult SearchWord(string str)
@@ -157,72 +157,144 @@ namespace 類義語とか調べるやつ
             var list = reader.GetValueList(keys).Select(x => x[0]).ToList();
             return new SearchSenceWordidResult(list);
         }
+
+
     }
+
+
+    public class DataBaseManager
+    {
+        static SQLiteUtil util = new SQLiteUtil("wnjpn.db");
+        public class Word
+        {
+            public string str;
+            public string id;
+            public string pos;
+
+            public Word(string str, string id, string pos)
+            {
+                Init(str, id, pos);
+            }
+
+            private void Init(string str, string id, string pos)
+            {
+                this.str = str;
+                this.id = id;
+                this.pos = pos;
+            }
+            public Word(string wordid)
+            {
+                var tmp = util
+                    .SearchWordName(wordid)
+                    .GetResult
+                    .First();
+                Init(tmp.lemma, tmp.wordid, tmp.pos);
+            }
+
+            public static IEnumerable<Word> GetWords(string word) =>
+                util
+                .SearchWord(word)
+                .GetResult
+                .Select(x => new Word(x.lemma, x.wordid, x.pos));
+
+            public void View() =>
+                Console.WriteLine($"単語名={str} ID={id} Pos={pos}");
+            public string Name() => str;
+
+            public IEnumerable<Syn> GetSyns() =>
+             util.SearchSenseSynSet(id)
+                     .SynSetArray
+                     .Select(x => new Syn(x));
+        }
+
+        public class Syn
+        {
+            public string SynStr;
+            public List<Word> wordList = new List<Word>();
+            public Syn(string synstr)
+            {
+                this.SynStr = synstr;
+                this.wordList = util
+                     .SearchSenseWord(synstr)
+                     .WordidArray
+                     .Select(x => new Word(x))
+                     .ToList();
+            }
+
+            public static IEnumerable<Syn> GetSyns(Word word) =>
+                util.SearchSenseSynSet(word.id)
+                    .SynSetArray
+                    .Select(x => new Syn(x));
+
+            public void View()
+            {
+                Console.WriteLine($"syn={SynStr}");
+                var wordStr = string.Join(" ", wordList.Select(x => x.Name()));
+                Console.WriteLine(wordStr);
+            }
+
+            public IEnumerable<string> GetWordNames =>
+                wordList.Select(x => x.Name());
+        }
+        public class Result
+        {
+            IEnumerable<Word> words;
+            IEnumerable<IEnumerable<Syn>> synSets;
+            public Result(string wordstr)
+            {
+                words = Word.GetWords(wordstr);
+                synSets = words.Select(x => x.GetSyns());
+            }
+            public IEnumerable<(Word word, IEnumerable<Syn> synSet)> GetResult() =>
+                words.Zip(synSets, (word, synSet) => (word, synSet));
+            public IEnumerable<string> GetWordNames()
+            {
+                var tmp = synSets.Select(x => x.Select(y => y.GetWordNames));
+                List<string> list = new List<string>();
+                foreach (var n in tmp)
+                    foreach (var m in n)
+                        foreach (var o in m)
+                            list.Add(o);
+                return list.Distinct();
+            }
+        }
+
+
+
+        public void Search(string str)
+        {
+            var result = new Result(str);
+
+            Console.WriteLine(string.Join(" ", result.GetWordNames()));
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var res in result.GetResult())
+            {
+                res.word.View();
+                foreach (var syn in res.synSet)
+                {
+                    Console.WriteLine();
+                    syn.View();
+                }
+            }
+        }
+    }
+
+
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            SQLiteUtil util = new SQLiteUtil("wnjpn.db");
+            DataBaseManager manager = new DataBaseManager();
             while (true)
             {
                 string str = Console.ReadLine();
                 Console.Clear();
                 Console.WriteLine("検索文字");
                 Console.WriteLine(str);
-                SQLiteUtil.SearchWordResult wordResult = util.SearchWord(str);
-              
-                var synset = wordResult.GetResult
-                    .Select(x => x.wordid)
-                    .Select(x => util.SearchSenseSynSet(x))
-                    .Select(x => x.SynSetArray)
-                    .ToArray();
-
-                var synwordidSet = synset
-                    .Select(x => (
-                    sysnset: x,
-                    wordidset: x.Select(y => util.SearchSenseWord(y)).ToArray()))
-                    .ToArray();
-
-                var bviewData = wordResult.GetResult
-                    .Zip(
-                    synwordidSet,
-                    (x, y) => (word: x, synset: y.sysnset.Zip(y.wordidset, (z, w) => (synset: z, wordidset: w))))
-                    .ToArray();
-
-                var viewData = bviewData
-                    .Select(x => (word: x.word, reslt: x.synset.Select(y => (y.synset, wordNameset: y.wordidset.WordidArray.Select(w => util.SearchWordName(w).WordidArray[0])))));
-
-                Console.WriteLine();
-
-                Console.Write("ヒットした結果");
-                var words = viewData.Select(x => x.reslt.Select(y => y.wordNameset.ToArray()).ToArray()).ToArray();
-
-                List<string> wordlist = new List<string>();
-                foreach (var n in words)
-                    foreach (var m in n)
-                        foreach (var o in m)
-                            wordlist.Add(o);
-                wordlist = wordlist.Distinct().ToList();
-
-                Console.WriteLine($" {wordlist.Count}件");
-                
-                Console.WriteLine(string.Join("\t", wordlist));
-
-                Console.WriteLine();
-
-                foreach (var n in viewData)
-                {
-                    Console.WriteLine(n.word);
-                    Console.WriteLine("類義概念単語群");
-                    foreach (var m in n.reslt)
-                    {
-                        Console.WriteLine("概念名");
-                        Console.WriteLine(m.synset);
-                        Console.WriteLine("所属単語");
-                        Console.WriteLine(string.Join(" ",m.wordNameset));
-                    }
-                }
+                manager.Search(str);
             }
         }
     }
